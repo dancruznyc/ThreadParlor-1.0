39fe14aa14fa66f33cf44eac08b90d18f9481885
dancruznyc <dancruznyc@gmail.com>
2022-04-21 19:19:42 -0400

    start carousel

--- frontend/src/components/Carousel.js
+++ frontend/src/components/Carousel.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Carousel = () => {
-  return <div>Carousel</div>;
+  const carouselImgs = [];
+  return (
+    <div className="carousel--container">
+      <div className="carousel-img--prev"></div>
+      <div className="carousel-img--curr"></div>
+      <div className="carousel-img--next"></div>
+    </div>
+  );
 };
 
 export default Carousel;
